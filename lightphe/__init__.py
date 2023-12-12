f0f63bed99f811db1c48e0ee7dd8d30ddda263d8
Sefik Ilkin Serengil <serengil@gmail.com>
2023-12-12 17:36:08 +0000

    encrypt decrypt tensor method added

--- lightphe/__init__.py
+++ lightphe/__init__.py
@@ -1,8 +1,10 @@
 import json
-from typing import Optional, Union
+from typing import Optional, Union, List
 
+from lightphe.models.Homomorphic import Homomorphic
 from lightphe.models.Ciphertext import Ciphertext
 from lightphe.models.Algorithm import Algorithm
+from lightphe.models.Tensor import EncryptedTensor, EncryptedTensors
 from lightphe.cryptosystems.RSA import RSA
 from lightphe.cryptosystems.ElGamal import ElGamal
 from lightphe.cryptosystems.Paillier import Paillier
@@ -12,12 +14,12 @@ from lightphe.cryptosystems.Benaloh import Benaloh
 from lightphe.cryptosystems.NaccacheStern import NaccacheStern
 from lightphe.cryptosystems.GoldwasserMicali import GoldwasserMicali
 from lightphe.cryptosystems.EllipticCurveElGamal import EllipticCurveElGamal
-from lightphe.commons import calculations
+from lightphe.commons import phe_utils
 from lightphe.commons.logger import Logger
 
-# pylint: disable=eval-used
+# pylint: disable=eval-used, simplifiable-if-expression
 
-logger = Logger()
+logger = Logger(module="lightphe/__init__.py")
 
 
 class LightPHE:
@@ -43,11 +45,11 @@ class LightPHE:
         if key_file is not None:
             keys = self.restore_keys(target_file=key_file)
 
-        self.cs = self.build_cryptosystem(
+        self.cs: Homomorphic = self.__build_cryptosystem(
             algorithm_name=algorithm_name, keys=keys, key_size=key_size
         )
 
-    def build_cryptosystem(
+    def __build_cryptosystem(
         self,
         algorithm_name: str,
         keys: Optional[dict] = None,
@@ -103,22 +105,29 @@ class LightPHE:
             raise ValueError(f"unimplemented algorithm - {algorithm_name}")
         return cs
 
-    def encrypt(self, plaintext: Union[int, float]) -> Ciphertext:
+    def encrypt(self, plaintext: Union[int, float, list]) -> Union[Ciphertext, EncryptedTensors]:
         """
         Encrypt a plaintext with a built cryptosystem
         Args:
-            plaintext (int or float): message
+            plaintext (int, float or tensor): message
         Returns
             ciphertext (from lightphe.models.Ciphertext import Ciphertext): encrypted message
         """
+        if self.cs.keys.get("private_key") is None:
+            raise ValueError("You must have private key to perform encryption")
+
+        if isinstance(plaintext, list):
+            # then encrypt tensors
+            return self.__encrypt_tensors(tensor=plaintext)
+
         ciphertext = self.cs.encrypt(
-            plaintext=calculations.parse_int(
-                value=plaintext, modulo=self.cs.modulo or self.cs.plaintext_modulo
-            )
+            plaintext=phe_utils.parse_int(value=plaintext, modulo=self.cs.plaintext_modulo)
         )
         return Ciphertext(algorithm_name=self.algorithm_name, keys=self.cs.keys, value=ciphertext)
 
-    def decrypt(self, ciphertext: Ciphertext) -> int:
+    def decrypt(
+        self, ciphertext: Union[Ciphertext, EncryptedTensors]
+    ) -> Union[int, List[int], List[float]]:
         """
         Decrypt a ciphertext with a buit cryptosystem
         Args:
@@ -126,7 +135,85 @@ class LightPHE:
         Returns:
             plaintext (int): restored message
         """
+        if self.cs.keys.get("private_key") is None:
+            raise ValueError("You must have private key to perform decryption")
+
+        if isinstance(ciphertext, EncryptedTensors):
+            # then this is encrypted tensor
+            return self.__decrypt_tensors(encrypted_tensor=ciphertext)
+
         return self.cs.decrypt(ciphertext=ciphertext.value)
+
+    def __encrypt_tensors(self, tensor: list) -> EncryptedTensors:
+        """
+        Encrypt a given tensor
+        Args:
+            tensor (list of int or float)
+        Returns
+            encrypted tensor (list of encrypted tensor object)
+        """
+        encrypted_tensor: List[EncryptedTensor] = []
+        for m in tensor:
+            sign = 1 if m >= 0 else -1
+            # get rid of sign anyway
+            m = m * sign
+            sign_encrypted = self.cs.encrypt(plaintext=sign)
+            if isinstance(m, int):
+                dividend_encrypted = self.cs.encrypt(plaintext=m)
+                divisor_encrypted = self.cs.encrypt(plaintext=1)
+                c = EncryptedTensor(
+                    dividend=dividend_encrypted,
+                    divisor=divisor_encrypted,
+                    sign=sign_encrypted,
+                )
+            elif isinstance(m, float):
+                dividend, divisor = phe_utils.fractionize(value=m, modulo=self.cs.plaintext_modulo)
+                dividend_encrypted = self.cs.encrypt(plaintext=dividend)
+                divisor_encrypted = self.cs.encrypt(plaintext=divisor)
+                c = EncryptedTensor(
+                    dividend=dividend_encrypted,
+                    divisor=divisor_encrypted,
+                    sign=sign_encrypted,
+                )
+            else:
+                raise ValueError(f"unimplemented type - {type(m)}")
+            encrypted_tensor.append(c)
+        return EncryptedTensors(encrypted_tensor=encrypted_tensor)
+
+    def __decrypt_tensors(
+        self, encrypted_tensor: EncryptedTensors
+    ) -> Union[List[int], List[float]]:
+        """
+        Decrypt a given encrypted tensor
+        Args:
+            encrypted_tensor (list of encrypted tensor)
+        Returns:
+            List of plain tensors
+        """
+        plain_tensor = []
+        for c in encrypted_tensor.encrypted_tensor:
+            if isinstance(c, EncryptedTensor) is False:
+                raise ValueError("Ciphertext items must be EncryptedTensor")
+
+            encrypted_dividend = c.dividend
+            encrypted_divisor = c.divisor
+            encrypted_sign = c.sign
+
+            dividend = self.cs.decrypt(ciphertext=encrypted_dividend)
+            divisor = self.cs.decrypt(ciphertext=encrypted_divisor)
+            sign = self.cs.decrypt(ciphertext=encrypted_sign)
+
+            if sign == self.cs.plaintext_modulo - 1:
+                sign = -1
+            elif sign == 1:
+                sign = 1
+            else:
+                raise ValueError("this cannot be true!")
+
+            m = sign * (dividend / divisor)
+
+            plain_tensor.append(m)
+        return plain_tensor
 
     def regenerate_ciphertext(self, ciphertext: Ciphertext) -> Ciphertext:
         """
@@ -136,6 +223,9 @@ class LightPHE:
         Returns:
             ciphertext (from lightphe.models.Ciphertext import Ciphertext): encrypted message
         """
+        if self.cs.keys.get("private_key") is None:
+            raise ValueError("You must have private key to perform decryption")
+
         ciphertext_new = self.cs.reencrypt(ciphertext=ciphertext.value)
         return Ciphertext(
             algorithm_name=self.algorithm_name, keys=self.cs.keys, value=ciphertext_new
