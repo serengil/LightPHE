6a96e7aa840b9fe500d422bc4f1ea3c3a41076e1
Sefik Ilkin Serengil <serengil@gmail.com>
2025-11-29 16:00:26 +0000

    increase version just after deployment

--- lightphe/__init__.py
+++ lightphe/__init__.py
@@ -34,7 +34,7 @@ from lightphe.cryptosystems.EllipticCurveElGamal import EllipticCurveElGamal
 
 logger = Logger(module="lightphe/__init__.py")
 
-VERSION = "0.0.19"
+VERSION = "0.0.20"
 
 
 class LightPHE:
